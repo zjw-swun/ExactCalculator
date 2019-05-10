0b5b44a92a6668a0860fad97c41b3568bc6bcf6e
markgray <mark.t.gray@gmail.com>
2019-05-10 05:08:27 -0400

    Added comment and some minor code cleanups.

--- app/src/main/java/com/example/calculator2/AlertDialogFragment.kt
+++ app/src/main/java/com/example/calculator2/AlertDialogFragment.kt
@@ -59,7 +59,17 @@ class AlertDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
      * We override this to build our own custom [Dialog] container. If there were no arguments supplied
      * when we were created (*getArguments* returns *null*) we inintialize our variable *args* to
      * Bundle.EMPTY, otherwise we initialize it to the arguments supplied when the fragment was
-     * instantiated.
+     * instantiated. We initialize our variable *builder* with a new instance of *AlertDialog.Builder*
+     * that uses *activity* as the parent context and initialize our variable *inflater* with a handle
+     * to the [LayoutInflater] for the context of the *builder* (which is the [FragmentActivity] this
+     * fragment is currently associated with). We then use *inflater* to inflate our layout file
+     * R.layout.dialog_message into our [TextView] variable *messageView*, set its *text* to the
+     * [CharSequence] in *args* stored under the key KEY_MESSAGE, and then set the view of *builder*
+     * to *messageView*. We initialize our variable *positiveButtonLabel* with the [CharSequence]
+     * stored in *args* under the key KEY_BUTTON_POSITIVE, and if this is not null we set the positive
+     * button of *builder* to have *positiveButtonLabel* as its text and *this* as its [OnClickListener].
+     * We then set the title of *builder* to the [CharSequence] stored under the key KEY_TITLE in
+     * *args* and return the [AlertDialog] built by the *create* method of *builder* to the caller.
      *
      * @param savedInstanceState The last saved instance state of the *Fragment* or null if this is
      * a freshly created *Fragment*.
@@ -71,7 +81,7 @@ class AlertDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
 
         val inflater = LayoutInflater.from(builder.context)
         @SuppressLint("InflateParams")
-        val messageView = inflater.inflate(R.layout.dialog_message, null/* root */) as TextView
+        val messageView = inflater.inflate(R.layout.dialog_message, null) as TextView
         messageView.text = args.getCharSequence(KEY_MESSAGE)
         builder.setView(messageView)
 
@@ -87,6 +97,15 @@ class AlertDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
         return builder.create()
     }
 
+    /**
+     * This method will be invoked when a button in the dialog is clicked. We initialize our variable
+     * *activity* to the [FragmentActivity] this fragment is currently associated with. If *activity*
+     * is an *AlertDialogFragment* *OnClickListener* we call its *onClick* method with *this* as the
+     * [AlertDialogFragment] that received the click, and [which] as the button that was clicked.
+     *
+     * @param dialog the dialog that received the click.
+     * @param which the button that was clicked.
+     */
     override fun onClick(dialog: DialogInterface, which: Int) {
         val activity = activity
         if (activity is OnClickListener /* always true */) {
@@ -94,6 +113,9 @@ class AlertDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
         }
     }
 
+    /**
+     * Contains our constants and static factory methods.
+     */
     companion object {
 
         private val NAME = AlertDialogFragment::class.java.name
@@ -103,34 +125,55 @@ class AlertDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
         private val KEY_TITLE = NAME + "_title"
 
         /**
-         * Convenience method for creating and showing a DialogFragment with the given message and
-         * title.
+         * Convenience method for creating and showing a [DialogFragment] with the given message and
+         * title string resource id's. We just fetch the strings that are stored in our APK for the
+         * resource id's [title], [message] and [positiveButtonLabel] (if they are not 0) then call
+         * the implementation of [showMessageDialog] which uses strings with these strings instead
+         * of the resource id's.
          *
-         * @param activity originating Activity
+         * @param activity originating [FragmentActivity]
          * @param title resource id for the title string
          * @param message resource id for the displayed message string
-         * @param positiveButtonLabel label for second button, if any.  If non-null, activity must
-         * implement AlertDialogFragment.OnClickListener to respond.
+         * @param positiveButtonLabel label for second button, if any. If non-null, activity must
+         * implement *AlertDialogFragment.OnClickListener* to respond to that button being clicked.
+         * @param tag tag for the *Fragment* that the *FragmentManager* will add.
          */
-        fun showMessageDialog(activity: FragmentActivity, @StringRes title: Int,
-                              @StringRes message: Int, @StringRes positiveButtonLabel: Int, tag: String?) {
-            showMessageDialog(activity, if (title != 0) activity.getString(title) else null,
+        fun showMessageDialog(activity: FragmentActivity,
+                              @StringRes title: Int,
+                              @StringRes message: Int,
+                              @StringRes positiveButtonLabel: Int,
+                              tag: String?) {
+            showMessageDialog(activity,
+                    if (title != 0) activity.getString(title) else null,
                     activity.getString(message),
                     if (positiveButtonLabel != 0) activity.getString(positiveButtonLabel) else null,
                     tag)
         }
 
         /**
-         * Create and show a DialogFragment with the given message.
+         * Create and show a DialogFragment with the given message. We initialize our variable
+         * *manager* to a handle to the FragmentManager for interacting with fragments associated
+         * with [activity]. If *manager* has been destroyed we return having done nothing. We
+         * initialize our variable *dialogFragment* with a new instance of [AlertDialogFragment],
+         * and our variable *args* with a new instance of [Bundle]. We add [message] to *args* under
+         * the key KEY_MESSAGE, add the string with resource id R.string.dismiss ("Dismiss") to *args*
+         * under the key KEY_BUTTON_NEGATIVE, add [positiveButtonLabel] to *args* under the key
+         * KEY_BUTTON_POSITIVE (if it is not null), and add [title] to *args* under the key KEY_TITLE.
+         * We then set the arguments of *dialogFragment* to *args*, and call its *show* method to
+         * have *manager* display the dialog.
          *
          * @param activity originating Activity
          * @param title displayed title, if any
          * @param message displayed message
-         * @param positiveButtonLabel label for second button, if any.  If non-null, activity must
-         * implement AlertDialogFragment.OnClickListener to respond.
+         * @param positiveButtonLabel label for second button, if any. If non-null, activity must
+         * implement *AlertDialogFragment.OnClickListener* to respond to that button being clicked.
+         * @param tag tag for the *Fragment* that the *FragmentManager* will add.
          */
-        fun showMessageDialog(activity: FragmentActivity, title: CharSequence?,
-                              message: CharSequence, positiveButtonLabel: CharSequence?, tag: String?) {
+        fun showMessageDialog(activity: FragmentActivity,
+                              title: CharSequence?,
+                              message: CharSequence,
+                              positiveButtonLabel: CharSequence?,
+                              tag: String?) {
             val manager = activity.supportFragmentManager
             if (manager.isDestroyed) {
                 return
