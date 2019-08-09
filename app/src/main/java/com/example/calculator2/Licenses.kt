75c63c167f3216fa40cd31748b69023ce4283a0f
markgray <mark.t.gray@gmail.com>
2019-08-09 06:26:42 -0400

    Added comments.

--- app/src/main/java/com/example/calculator2/Licenses.kt
+++ app/src/main/java/com/example/calculator2/Licenses.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.webkit.WebView
 
+/**
+ * This activity is launched when the user selects the "Open source licenses" menu option in the
+ * [Calculator2] option menu, and just displays the file licenses.html from our assets in a
+ * [WebView].
+ */
 class Licenses : Activity() {
-
+    /**
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use this.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
