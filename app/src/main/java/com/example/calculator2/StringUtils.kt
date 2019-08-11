d1707232de68690b1cf5176e042125a1c6eab798
markgray <mark.t.gray@gmail.com>
2019-08-11 05:06:13 -0400

    Added comments.

--- app/src/main/java/com/example/calculator2/StringUtils.kt
+++ app/src/main/java/com/example/calculator2/StringUtils.kt
@@ -38,12 +38,26 @@ object StringUtils {
     }
 
     /**
-     * Return a copy of the supplied string with commas added every three digits.
-     * The substring indicated by the supplied range is assumed to contain only
-     * a whole number, with no decimal point.
-     * Inserting a digit separator every 3 digits appears to be
-     * at least somewhat acceptable, though not necessarily preferred, everywhere.
-     * The grouping separator in the result is NOT localized.
+     * Return a copy of the supplied string with commas added every three digits. The substring
+     * indicated by the supplied range is assumed to contain only a whole number, with no decimal
+     * point. Inserting a digit separator every 3 digits appears to be at least somewhat acceptable,
+     * though not necessarily preferred, everywhere. The grouping separator in the result is NOT
+     * localized.
+     *
+     * First we initialize our `val result` with a new instance of [StringBuilder]. We initialize
+     * our `var current` to [begin], then while `current` is less than [end] and the `current`
+     * character in [s] is either an '-' or an ' ' character we increment `current` to skip it.
+     * We then append the characters between [begin] and `current` in [s] to `result`. Next we loop
+     * while `current` is less than [end] appending the `current` character in [s] to `result` then
+     * incrementing `current`. If the characters remaining ([end] minus `current`) modulo 3 is 0 and
+     * [end] is not equal to `current` we also append a ',' character to `result`.
+     *
+     * When done (`current` is not equal to [end]) we return the string value of `result`.
+     *
+     * @param s String to have commas inserted into.
+     * @param begin Starting index of substring of [s] to consider.
+     * @param end Ending index of substring of [s] to consider.
+     * @return Copy of [s] with commas inserted every three digits.
      */
     fun addCommas(s: String, begin: Int, end: Int): String {
         // Resist the temptation to use Java's NumberFormat, which converts to long or double
@@ -65,11 +79,34 @@ object StringUtils {
     }
 
     /**
-     * Ignoring all occurrences of c in both strings, check whether old is a prefix of new.
-     * If so, return the remaining sub-sequence of whole. If not, return null.
+     * Ignoring all occurrences of [c] in both strings, check whether [prefix] is a prefix of
+     * [whole]. If so, return the remaining sub-sequence of [whole]. If not, return null. We
+     * initialize our `var wIndex` to 0 (our character index into [whole]), our `var pIndex`
+     * to 0 (our character index into [prefix]), our `val wLen` to the length of [whole], and
+     * our `val pLen` to the length of [prefix]. We then loop forever:
+     * - We increment `pIndex` as long as the `pIndex` character of [prefix] is [c].
+     * - We increment `wIndex` as long as the `wIndex` character of [whole] is [c].
+     * - If `pIndex` is equal to `pLen` we break out of the loop.
+     * - If `wIndex` is equal to `wLen` or the `wIndex` character of [whole] is not equal to the
+     * `pIndex` character of [prefix] we return *null* (there are either no new characters at the
+     * end of [whole] or [prefix] is not a prefix of [whole] after all).
+     * - We increment both `pIndex` and `wIndex` and loop around to compare the next character in
+     * [prefix] and [whole].
+     *
+     * If we break out of the infinite loop without returning *null* there are new characters at
+     * the end of [whole], so we first increment `wIndex` to skip any [c] characters in [whole],
+     * then return the subsequence of [whole] from `wIndex` to `wLen` to the caller.
+     *
+     * @param whole String to examine for [prefix], and whose extension to [prefix] to return.
+     * @param prefix Old string which might be extended in [whole].
+     * @param c Character to ignore while comparing (the separator character in our case).
+     * @return String of characters added to the end of [prefix] to create [whole] or *null*.
      */
-    fun getExtensionIgnoring(whole: CharSequence, prefix: CharSequence,
-                             c: Char): CharSequence? {
+    fun getExtensionIgnoring(
+            whole: CharSequence,
+            prefix: CharSequence,
+            c: Char
+    ): CharSequence? {
         var wIndex = 0
         var pIndex = 0
         val wLen = whole.length
